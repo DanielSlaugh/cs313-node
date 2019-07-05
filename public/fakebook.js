33c093757153775d31f513d5b4994a1f5e26d2be
DanielSlaugh <sla15004@byui.edu>
2019-07-05 12:29:26 -0600

    m

--- public/fakebook.js
+++ public/fakebook.js
@@ -1,5 +1,5 @@
 console.log("in the script");
-var valid_user = false;
+var val = false;
 
 // Calls every time the page has been loaded. Fixes the bug where user
 // couldn't do anything after signing up because the HTML hadn't been created
@@ -37,7 +37,7 @@ function load_home_page(valid_user) {
                   if (document.getElementById("login_uname").value == data.rows[i].username &&
                       document.getElementById("login_psw").value == data.rows[i].password) {
                          alert("Checked user against the database!! Logged in");
-                        //  val = true;
+                         val = true;
                          valid_user = 1;
                   }
                   else{
