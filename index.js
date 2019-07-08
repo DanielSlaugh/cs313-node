cde2cb67ef3eecbb663dfe37c8415c4b1b63d8ea
DanielSlaugh <sla15004@byui.edu>
2019-07-08 09:39:02 -0600

    k

--- index.js
+++ index.js
@@ -50,6 +50,9 @@ express()
       var sql = "SELECT u.id, u.display_name, u.username, u.password FROM users u WHERE u.username='" + uname + "' AND u.password='" + psw + "'";
       pool.query(sql, function (err, result) {
          var length = result.rows.length
+        console.log(result.rows)
+        console.log(uname)
+         console.log(psw)
          if (length == 0) {
            req.session.val = 0;
            res.json({val: 0})
