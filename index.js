87e5102e597666bae6d6f5481614d4e5fba4712f
DanielSlaugh <sla15004@byui.edu>
2019-07-11 17:36:57 -0600

    n

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ express()
   .post("/new_comment", (req, res) => {
     var new_comment = req.body.comment_text
     console.log(new_comment)
-    var sql = "INSERT INTO comment (message_id, comment_text) VALUES ('" + req.session.message_id + "', '" + new_comment + "')";
+    var sql = "INSERT INTO comment (message_id, comment_text) VALUES (" + req.session.message_id + ", '" + new_comment + "')";
     pool.query(sql, function (err, result) {
       res.json({ new_comment: new_comment })
     })
