e4d83b3377dde1b184d7d44ad1b02819a1e68954
DanielSlaugh <sla15004@byui.edu>
2019-07-11 20:12:00 -0600

    j

--- public/fakebook.js
+++ public/fakebook.js
@@ -130,7 +130,7 @@ function goto_comments(i) {
             for (var k = 0; k < comment_data.result.rows.length; k++) {
                if (message_id == comment_data.result.rows[k].id) {
                   current_user_id = comment_data.result.rows[k].user_id;
-                  current_user_comment = comment_data.result.rows[current_user_id].comment_text;
+                  current_user_comment = comment_data.result.rows[k].comment_text;
 
                   $.post("/getUserList", {current_user_id: current_user_id}, function (users_data, status) {
                      var comment_display_name = users_data.result.rows[0].display_name;
